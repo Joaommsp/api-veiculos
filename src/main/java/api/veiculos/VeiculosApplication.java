84fdd2443b4f880fe4a3b0731cafab8fecde8e24
Joao <jmmsp2003@hotmail.com>
2024-03-01 21:36:15 -0300

    primeiro commit

---
+++ src/main/java/api/veiculos/VeiculosApplication.java
@@ -0,0 +1,15 @@
+package api.veiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeiculosApplication.class, args);
+	}
+
+
+
+}
